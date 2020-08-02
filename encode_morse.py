3ccfd407f49ae33ff35ebbfc01113d1fea040898
mustafa <mustafaankarali35@gmail.com>
2020-08-03 00:36:15 +0200

    encode_morse.py added.

---
+++ encode_morse.py
@@ -0,0 +1,28 @@
+"""
+Create a function that takes a string as an argument and return a non-encoded, encrypted string.
+
+Notes
+Input value can be lower or upper case.
+Input string can have digits.
+Input string can have some special characters (e.g. comma, colon, apostrophe, period, question mark, exclamation mark).
+"""
+
+
+def encode_morse(message):
+	char_to_dots = {
+  'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+  'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+  'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+  'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+  'Y': '-.--', 'Z': '--..', ' ': ' ', '0': '-----',
+  '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+  '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+  '&': '.-...', "'": '.----.', '@': '.--.-.', ')': '-.--.-', '(': '-.--.',
+  ':': '---...', ',': '--..--', '=': '-...-', '!': '-.-.--', '.': '.-.-.-',
+  '-': '-....-', '+': '.-.-.', '"': '.-..-.', '?': '..--..', '/': '-..-.'
+	}
+	A=""
+	for i in message.upper():
+		A+=' '
+		A+= char_to_dots.get(i)
+	return A[1:]
