9ce9dac1edd781fdd35a415df57539098816b7be
mustafa <mustafaankarali35@gmail.com>
2020-08-03 00:33:41 +0200

    Encryption_algorithm.py added.

---
+++ Encryption_algorithm.py
@@ -0,0 +1,41 @@
+"""
+Make a function that encrypts a given input with these steps:
+
+Input: "apple"
+
+Step 1: Reverse the input: "elppa"
+
+Step 2: Replace all vowels using the following chart:
+
+a => 0
+e => 1
+i => 2
+o => 2
+u => 3
+
+# "1lpp0"
+Step 3: Add "aca" to the end of the word: "1lpp0aca"
+
+Output: "1lpp0aca"
+
+Examples
+encrypt("banana") ➞ "0n0n0baca"
+
+encrypt("karaca") ➞ "0c0r0kaca"
+
+encrypt("burak") ➞ "k0r3baca"
+
+encrypt("alpaca") ➞ "0c0pl0aca"
+Notes
+All inputs are strings, no uppercases and all output must be strings.
+"""
+
+
+
+
+def encrypt(word):
+    encrypted = word[::-1].replace("a","0").replace("e","1").replace("i","2").replace("o","2").replace("u","3")+"aca"
+    return encrypted
+
+
+encrypt("banana")
