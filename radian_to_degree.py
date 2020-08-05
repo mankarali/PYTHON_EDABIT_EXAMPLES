89d1f28d932adbeab9bf183ccb0e36bff860f5b0
mustafa <mustafaankarali35@gmail.com>
2020-08-05 23:35:06 +0200

    radian_to_degree.py added.

---
+++ radian_to_degree.py
@@ -0,0 +1,20 @@
+"""
+Radian to Degree
+Create a function that takes an angle in radians and converts it into degrees.
+Examples
+to_degree(math.pi) ➞ 180
+
+to_degree(math.pi/2) ➞ 90
+
+to_degree(math.pi/4) ➞ 45
+"""
+
+import math
+def to_degree(radian):
+    pi=math.pi
+    degree=(radian*180)/pi
+    return (degree)
+to_degree(math.pi)
+
+
+to_degree(math.pi/4)
