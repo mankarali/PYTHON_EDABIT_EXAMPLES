cef45b021c4b62628b23452ed80bb50f024f9c0d
mustafa <mustafaankarali35@gmail.com>
2020-08-06 23:04:38 +0200

    count_letters_in_word_search.py added.

---
+++ count_letters_in_word_search.py
@@ -0,0 +1,40 @@
+"""
+Count Letters in a Word Search
+Create a function that counts the number of times a particular letter shows up in the word search.
+
+Examples
+letter_counter([
+  ["D", "E", "Y", "H", "A", "D"],
+  ["C", "B", "Z", "Y", "J", "K"],
+  ["D", "B", "C", "A", "M", "N"],
+  ["F", "G", "G", "R", "S", "R"],
+  ["V", "X", "H", "A", "S", "S"]
+], "D") ➞ 3
+
+# "D" shows up 3 times: twice in the first row, once in the third row.
+
+letter_counter([
+  ["D", "E", "Y", "H", "A", "D"],
+  ["C", "B", "Z", "Y", "J", "K"],
+  ["D", "B", "C", "A", "M", "N"],
+  ["F", "G", "G", "R", "S", "R"],
+  ["V", "X", "H", "A", "S", "S"]
+], "H") ➞ 2
+"""
+
+def letter_counter(lst, letter):
+    count =0
+    for i in range(len(lst)):
+        for j in lst[i]:
+            if j == letter:
+                count = count +1
+
+    return count
+
+letter_counter([
+  ["D", "E", "Y", "H", "A", "D"],
+  ["C", "B", "Z", "Y", "J", "K"],
+  ["D", "B", "C", "A", "M", "N"],
+  ["F", "G", "G", "R", "S", "R"],
+  ["V", "X", "H", "A", "S", "S"]
+], "D")
