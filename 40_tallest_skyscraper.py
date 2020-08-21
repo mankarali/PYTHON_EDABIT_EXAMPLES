10a4216135a0d46861af16c85f45b8e520e48626
mustafa <mustafaankarali35@gmail.com>
2020-08-21 19:19:19 +0200

    40_tallest_skyscraper.py

---
+++ 40_tallest_skyscraper.py
@@ -0,0 +1,47 @@
+"""Tallest Skyscraper
+A city skyline can be represented as a 2-D list with 1s representing buildings. In the example below, the height of the tallest building is 4 (second-most right column).
+
+[[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 1, 0],
+[0, 0, 1, 0, 1, 0],
+[0, 1, 1, 1, 1, 0],
+[1, 1, 1, 1, 1, 1]]
+Create a function that takes a skyline (2-D list of 0's and 1's) and returns the height of the tallest skyscraper.
+
+Examples
+tallest_skyscraper([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 1, 0],
+  [1, 1, 1, 1]
+]) ➞ 3
+
+tallest_skyscraper([
+  [0, 1, 0, 0],
+  [0, 1, 0, 0],
+  [0, 1, 1, 0],
+  [1, 1, 1, 1]
+]) ➞ 4
+
+tallest_skyscraper([
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [1, 1, 1, 0],
+  [1, 1, 1, 1]
+]) ➞ 2
+"""
+
+def tallest_skyscraper(lst):
+    count = 0
+    for i in lst:
+        if not i.count(1) == 0:
+            count += 1
+    return count
+    
+    
+tallest_skyscraper([
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [1, 1, 1, 0],
+  [1, 1, 1, 1]
+])
